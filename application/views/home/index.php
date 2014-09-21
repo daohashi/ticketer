0544d23c0c7930c018ea52136a09128ab9303442
Donald Ngai <donaldtak@hotmail.com>
2014-09-21 03:02:33 -0400

    loading icon and login

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,12 +1,17 @@
-<div id="Coordinates">
+<div id="Login">
+
+<input type="text" name="pname" id="textbox">
+<input type="submit" name="psubmit" id="submitbutton">
+
 </div>
 <div class="Middle" id="icons">
-	<img src="public/img/settings.jpg" height="35px" width="35px" id="settings">
+	<img src="public/img/locked.png" height="35px" width="25px" id="settings">
+	<img src="public/img/unlocked.png" height="35px" width="35px" id="unlocked">
 </div>
 
 <div class="Middle" id="Events">
 	<ul type="none" id="Lists">
-
+<li><img src="public/img/ajax-loader.gif"></li>
 	</ul>
 
 </dl>

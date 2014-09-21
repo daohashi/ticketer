48248193bc4624c1dd9bc8b3b2100bb737f3a951
daohashi <ohashi.daniel@gmail.com>
2014-09-21 02:40:20 -0400

    description page

--- application/views/home/description.php
+++ application/views/home/description.php
@@ -1,19 +1,19 @@
 <div class="Middle" id="eventName">
 	<div class='mcontain'>
 		<div class='mcontaincell'>
-Hack the North
+<?php echo $event['name']; ?>
 </div>
 </div>
 </div>
 <div class="Middle" id="description">
 	<div class='mcontain'>
 		<div class='mcontaincell'>
-	Hack the North is Canada's premier hackathon. It's an event where 1,000 students with different technical backgrounds and skill levels will come together, for 36 hours, form teams around a problem or idea, and collaboratively code a unique solution from scratch. With world class mentors, food, and hardware resources, you're in for an amazing time!
+	<?php echo $event['description']; ?>
 </div>
 </div>
 </div>
 <div class="Middle" id="grabTicket">
-	<button type="button" class="btn" id="listButton">Grab Ticket</button>
+	<a href=<?php echo '"/home/getticket/' . $event['id'] . '"'?> class="btn" id="listButton">Grab Ticket</a>
 </div>
 
 
